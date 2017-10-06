551398c6fce3b1e55065097dcea0645dd252a175
pkouris <pkouris@islab.ntua.gr>
2017-10-07 00:26:04 +0300

    xlsx_to_txt update for 20 files

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-win_dataset_path = "E:/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/"
-lin_dataset_path = "/home/pkouris/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/"
+win_dataset_path = "E:/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/txt/"
+lin_dataset_path = "/home/pkouris/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/txt/"
 
 win_statistics_path = "E:/Dropbox/EMP_DID_dropbox/NLP/SIMILES/statistics/"
 lin_statistics_path = "/home/pkouris/Dropbox/EMP_DID_dropbox/NLP/SIMILES/Statistics/"
 
-dataset_path = lin_dataset_path
-statistics_path = lin_statistics_path
+dataset_path = win_dataset_path
+statistics_path = win_statistics_path
 
 
 
@@ -21,7 +21,18 @@ filenames = ["1_aspros_san_to_pani.txt",
              "7_oplismenos_san_astakos.txt",
              "8_malakos_san_voutiro.txt",
              "9_geros_san_tavros.txt",
-             "10_pistos_san_skilos.txt"]
+             "10_pistos_san_skilos.txt",
+             "11_kokkinos_san_paparouna.txt",
+             "12_ntimenos_san_astakos.txt",
+             "13_kokkinos_san_to_pantzari.txt",
+             "14_glikos_san_meli.txt",
+             "15_aspros_san_to_gala.txt",
+             "16_krios_san_ton_pago.txt",
+             "17_grigoros_san_astrapi.txt",
+             "18_mavros_san_skotadi.txt",
+             "19_mperdemenos_san_to_kouvari.txt",
+             "20_aspros_san_to_xioni.txt"
+             ]
 results_filenames = ["endropy_v3.txt"]
 
 
--- xlsx_to_txt.py
+++ xlsx_to_txt.py
@@ -1,87 +1,92 @@
 import xlrd
 import timeit
+
+
 class XlsxToTxt:
-
-
-
-
-
     Files = ["",
-                  "1_aspros_san_to_pani",
-                  "2_stolismenos_san_fregata",
-                  "3_apalos_san_poupoulo",
-                  "4_apalos_san_xadi",
-                  "5_elafrys_san_poupoulo",
-                  "6_kokkinos_san_astakos",
-                  "7_oplismenos_san_astakos",
-                  "8_malakos_san_voutiro",
-                  "9_geros_san_tavros",
-                  "10_pistos_san_skilos"]
+             "1_aspros_san_to_pani",
+             "2_stolismenos_san_fregata",
+             "3_apalos_san_poupoulo",
+             "4_apalos_san_xadi",
+             "5_elafrys_san_poupoulo",
+             "6_kokkinos_san_astakos",
+             "7_oplismenos_san_astakos",
+             "8_malakos_san_voutiro",
+             "9_geros_san_tavros",
+             "10_pistos_san_skilos",
+             "11_kokkinos_san_paparouna",
+             "12_ntimenos_san_astakos",
+             "13_kokkinos_san_to_pantzari",
+             "14_glikos_san_meli",
+             "15_aspros_san_to_gala",
+             "16_krios_san_ton_pago",
+             "17_grigoros_san_astrapi",
+             "18_mavros_san_skotadi",
+             "19_mperdemenos_san_to_kouvari",
+             "20_aspros_san_to_xioni"
+             ]
 
     linux_dataset_path = "/home/pkouris/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/"
     win_dataset_path = "E:/Dropbox/EMP_DID_dropbox/NLP/SIMILES/dataset/"
-    dataset_path = linux_dataset_path  #It should be changed according to pc
+    dataset_path = win_dataset_path  # It should be changed according to pc
     txt_datapath = dataset_path + "txt/"
 
-    cellsForTXT = [3, 5, 7, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22]
-    headers = ["", #0
-               "TEXT",#1
-               "SIMILE",#2
-               "GENDER",#3
-               "HEAD",#4
-               "LEMMA",#5
-               "MOD_PRED_SEMS",#6   MODIFIED PRED SEMS
-               "TEN_GEN_SEMS",#7  TENOR SEM GENERALISATION
-               "MWE_TYPE",#8
-               "PHENOMENON", #9
-               "DETERMINER",#10
-               "EMPM",#11
-               "EMPP",#12
-               "COMP", #13
-               "IWO",#14
-               "IXP-CREATIVE",#15
-               "IXP-EXPANSION",#16
-               "IXP-N_W_PUNC",#17
-               "MOD",#18
-               "AGR",#19
-               "MWO",#20
-               "VAR",#21
-               "AN"] #22
-
-
+    cellsForTXT = [3, 5, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23]
+    headers = ["",  # 0
+               "TEXT",  # 1
+               "SIMILE",  # 2
+               "GENDER",  # 3
+               "HEAD",  # 4
+               "LEMMA",  # 5
+               "MOD_PRED_SEMS",  # 6   MODIFIED PRED SEMS
+               "TEN_GEN_SEMS",  # 7  TENOR SEM GENERALISATION
+               "MWE_TYPE",  # 8
+               "PHENOMENON",  # 9
+               "DETERMINER",  # 10
+               "EMPM",  # 11
+               "EMPP",  # 12
+               "COMP",  # 13
+               "IWO",  # 14
+               "IXP-CREATIVE",  # 15
+               "IXP-EXPANSION",  # 16
+               "IXP-N",  # 17
+               "IXP-W", #18
+               "IXP-PUNC", #19
+               "MOD",  # 20
+               "AGR",  # 21
+               "MWO",  # 22
+               "VAR",  # 23
+               "AN"]  # 24
 
     def __init__(self):
         s = XlsxToTxt
         start = timeit.default_timer()
-        #s.xlsx_to_txt("self", specificFile=1, cellsForTxt=s.cellsForTXT)
-        s.convertAllXlsxToTXT(self)
-        #s.convertRangeOfXlsxToTXT(self, rangeOfXlsxFilse=[1,3,5])
-        print(str(round(timeit.default_timer()-start, 3)) + " sec")
-
+        # s.xlsx_to_txt("self", specificFile=1, cellsForTxt=s.cellsForTXT)
+        #s.convertAllXlsxToTXT(self)
+        s.convertRangeOfXlsxToTXT(self, rangeOfXlsxFilse=[20])
+        print(str(round(timeit.default_timer() - start, 3)) + " sec")
 
     def convertAllXlsxToTXT(self):
-        s = XlsxToTxt #self class
+        s = XlsxToTxt  # self class
         file_index = 0
         for f in s.Files:
             s.xlsx_to_txt("self", specificFile=file_index, cellsForTxt=s.cellsForTXT)
             file_index += 1
 
-
-    def convertRangeOfXlsxToTXT(self, rangeOfXlsxFilse = []):
+    def convertRangeOfXlsxToTXT(self, rangeOfXlsxFilse=[]):
         s = XlsxToTxt  # self class
         for file_index in rangeOfXlsxFilse:
             s.xlsx_to_txt("self", specificFile=file_index, cellsForTxt=s.cellsForTXT)
 
-
-    #It convert the scpecific xlsx file to txt
+    # It convert the scpecific xlsx file to txt
     def xlsx_to_txt(self, specificFile=0, cellsForTxt=[]):
-        s = XlsxToTxt  #self class
-        if specificFile>0:
+        s = XlsxToTxt  # self class
+        if specificFile > 0:
             txtFile = s.txt_datapath + "" + s.Files[specificFile] + ".txt"
             xlsxFile = s.dataset_path + "" + s.Files[specificFile] + ".xlsx"
             with open(txtFile, "w", encoding="utf-8") as wr:
                 xlsx = xlrd.open_workbook(xlsxFile, encoding_override='utf-8')
-                sheet = xlsx.sheet_by_index(0) # get a sheet
+                sheet = xlsx.sheet_by_index(0)  # get a sheet
                 flag = 2
                 numberOfXlsxRows = sheet.nrows
                 txtRow_index = 1
@@ -92,23 +97,21 @@ class XlsxToTxt:
                         for h in cellsForTxt:
                             wr.write("# " + s.headers[h])
                         wr.write("\n")
-                    if(xlsxRow_index > 4):
-                        flag +=1
+                    if (xlsxRow_index > 3):
+                        flag += 1
                         if flag == 3:
                             txtRow_index += 1
                             wr.write(str(specificFile) + " # " + str(xlsxRow_index) + " # " + str(txtRow_index))
                             for c in cellsForTxt:
-                                cellValue = str(sheet.cell(xlsxRow_index-1, c-1).value).strip()
+                                cellValue = str(sheet.cell(xlsxRow_index - 1, c - 1).value).strip()
                                 wr.write(" # " + cellValue)
                                 flag = 0
-                                if cellValue=="":
-                                    print("null cell:" + s.Files[specificFile]
-                                          + "Row: " + str(xlsxRow_index)
+                                if cellValue == "":
+                                    print("null cell: " + s.Files[specificFile]
+                                          + " Row: " + str(xlsxRow_index)
                                           + " Cell: " + s.headers[c])
                             wr.write("\n")
             wr.close()
-
-
 
 
 XlsxToTxt()
